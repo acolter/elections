e74bca4cb48831251523b72c8eb22c936235a970
Angela Colter <angela.colter@gsa.gov>
2019-12-27 09:42:13 -0500

    Use dplyr to replace "(" and ")"

--- oregon_canvass.R
+++ oregon_canvass.R
@@ -1,3 +1,7 @@
+## Cleaning output of numbered key canvass election results report 
+## read in the data but don't require the headers to be valid column names
+## use the second row for the column names and get rid of the first row
+
 library(tidyr)
 library(dplyr)
 library(stringr)
@@ -5,21 +9,15 @@ library(stringr)
 file <- "2000 Clackamas General - State Senate 28.csv"
 
 tidy_data <- function(file) {
-
-## read in the data but don't require the headers to be valid column names
-## use the second row for the column names and get rid of the first row
 df <- read.csv(file, stringsAsFactors = FALSE, header = FALSE)
+
+## set column and variable names
 colnames(df) <- df[1,]
-
-## read in values for office, district and county from file name
-office_pattern <- "President|U.S. |Secretary of |State |House|Senate|Governor|Attorney General| Treasurer"
-office <- paste(str_extract_all(file, office_pattern, simplify = TRUE), collapse="")
-
-string <- file %>%
-  str_match("(\\d+)(-R)*.csv")
-district <- string[,2]
-candidate <- tail(names(df),-1)
+offices <- "President|U.S. |Secretary of |State |House|Senate|Governor|Attorney General| Treasurer"
 county <- word(file,2)
+office <- paste(str_extract_all(file, offices, simplify = TRUE), collapse="")
+district <- str_match(file,"(\\d+)(-R)*.csv")[,2]
+candidate <- tail(names(df),-1)
 
 ## reshape wide format to long format
 df <- df %>% 
@@ -30,14 +28,11 @@ df <- df %>%
          office = office, 
          district = district,
          party = party) %>%
-  select(county,precinct,office,district,party,candidate,votes) 
-
-## clean up errant periods and NA values
-df$party <- str_remove(df$party, "\\)")
-df$party <- str_remove(df$party, "\\(")
-df[is.na(df)] <- ""
+  mutate(district = replace_na(district, "")) %>%
+  mutate(party = str_replace_all(party, "\\(|\\)","")) %>%
+  select(county, precinct, office, district, party, candidate, votes) 
+  
 write.csv(df, file=paste0(county,".csv"), row.names = F)
-
 return(df)
 
 }
