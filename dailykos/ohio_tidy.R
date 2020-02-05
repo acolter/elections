254a7f1025377605006e64ec4b8026fbcdde4e5c
Angela Colter <acolter@gmail.com>
2020-02-04 19:58:53 -0500

    Add files via upload

--- dailykos/ohio_tidy.R
+++ dailykos/ohio_tidy.R
@@ -8,41 +8,34 @@ file <- "2018-11-06_statewideprecinct.xlsx"
 excel_sheets(file)
 
 ## governor results
-df <- read_excel(file, sheet=3)
+df <- read_xlsx(file, sheet=3, skip=1, col_names=TRUE)
 gov_tidy <- df %>% 
-  setNames(df[1,]) %>%
   select(c(1,2,9:15)) %>%
-  slice(-c(1:3)) %>% 
-  mutate_at(vars(3:9), as.numeric) %>% 
+  slice(-c(1:2)) %>% 
   mutate(precinct_total = rowSums(.[3:9])) %>%
   select(-c(3,6:9)) %>%
+  mutate(office = "governor") %>%
   rename(county = "County Name",
          precinct = "Precinct Name",
          Cordray = "Richard Cordray and Betty Sutton (D)",
          DeWine = "Mike DeWine and Jon Husted (R)") %>%
-  gather(key = gov_candidate, value = gov_votes, Cordray:precinct_total) %>%
-  mutate(office = "governor") %>%
-  select(county, precinct, office, gov_candidate, gov_votes)
+  gather(key = gov_candidate, value = gov_votes, Cordray:precinct_total)
 
 ## state legislative results
-df <- read_excel(file, sheet=5,col_names = FALSE)
-districts <- as.character(df[1,]) 
-districts <- na.locf(districts) 
-df[1,] <- districts 
-combined_names <- paste(df[1,], df[2,], sep="-")
-df[1,] <- combined_names
+df <- read_xlsx(file, sheet=5, col_names = FALSE)
+df[1,] <- paste(na.locf(as.character(df[1,])), df[2,], sep="-")
 df[1,1:2] <- c("county","precinct")
 
 ga_tidy <- df %>%
   select(-c(3:44)) %>%
-  slice(-c(3:4)) %>%
+  slice(-c(2:4)) %>%
   mutate_all(funs(str_replace(., "State Representative - District ", "")))  
 
 colnames(ga_tidy) <- ga_tidy[1,]
 house_candidate <- tail(names(ga_tidy),-2)
 
 ga_tidy <- ga_tidy %>% 
-  slice(-c(1:2)) %>% 
+  slice(-1) %>% 
   gather(house_candidate, key = house_candidate, value = house_votes) %>%
   separate(house_candidate, sep = 3, into = c("house_district","house_candidate")) %>%
   mutate(house_district = str_replace(house_district, "-","")) %>%
@@ -50,9 +43,6 @@ ga_tidy <- ga_tidy %>%
   mutate_at(vars(5), as.numeric) %>%
   drop_na()
 
-write.csv(ga_tidy, file=("ga_tidy.csv"), row.names = F)
-
 ## join the two sheets
 join_tidy <- full_join(ga_tidy,gov_tidy,by=c("county","precinct"))
 write.csv(join_tidy, file=("join_tidy.csv"), row.names = F)
-
--- dailykos/undervote_factor.R
+++ dailykos/undervote_factor.R
@@ -1,141 +1,128 @@
 ## unsplit totals for gov and house
 
 join_tidy <- read.csv("join_tidy.csv", stringsAsFactors = FALSE)
-join_tidy <- mutate_at(join_tidy,vars(house_district),funs(as.character))
-ga_tidy <- read.csv("ga_tidy.csv", stringsAsFactors = FALSE)
 
-county_tabs <- join_tidy %>%
+### UNADJUSTED NUMBERS FOR COUNTY TABS ###
+county_tab_df <- join_tidy %>%
   select(-c(house_candidate:office)) %>%
   distinct() %>%
-  spread(key=gov_candidate,value=gov_votes)
+  spread(key=gov_candidate,value=gov_votes) 
 
-write.csv(county_tabs, file="county_tabs.csv", row.names = F)
+by_county <- split(county_tab_df,county_tab_df$county)
+sapply(names(by_county), 
+       function (x) write.csv(by_county[[x]], file=paste0("counties/",x,".csv"), row.names=FALSE))
 
-split_precinct <- join_tidy %>% 
-  mutate_at(vars(gov_votes,house_votes),funs(as.numeric)) %>%
-  #mutate_at(vars(house_district),funs(as.character)) %>%
+## unadjusted numbers for gov race ##
+gov_complete_df <- join_tidy %>%
+  select(-c(house_district:office)) %>%
+  distinct()
+
+
+### UNSPLIT PRECINCTS FOR TABLE TAB ###
+
+## list of split precincts 
+split_precinct_df <- join_tidy %>% 
   group_by(county,precinct) %>% 
-  select(-c(4:8)) %>%
+  select(-c(house_candidate:gov_votes)) %>%
   distinct(county,precinct,house_district, .keep_all = TRUE) %>%
-  filter(n()>1) %>%
-  arrange(desc(precinct), .by_group = TRUE)
+  filter(n()>1)
 
-## There are 255 split precincts in Ohio 
-split_precincts <- split_precinct[["precinct"]]
+## There are 255 split precincts in 21 counties 
+split_precincts <- sort(split_precinct_df[["precinct"]])
+split_precincts_counties <- sort(unique(split_precinct_df[["county"]]))
 
-### calculates governor votes in unsplit precincts 
-gov_unsplit <- join_tidy %>% 
+## calculates governor votes in unsplit precincts
+gov_unsplit_df <- join_tidy %>% 
   select(-c(house_candidate,office)) %>%
   distinct(county,precinct,house_district,gov_candidate,gov_votes) %>%
   mutate(gov_votes = ifelse(precinct %in% split_precincts, NA, gov_votes)) %>%
   spread(key=gov_candidate,value=gov_votes) %>%
   group_by(county,house_district) %>%
-  summarize(total_cordray = sum(Cordray), 
-            total_dewine = sum(DeWine), 
-            total_precinct = sum(precinct_total))
-
-gov_unsplit_export <- gov_unsplit %>%
-  mutate(shortname = ifelse(is.na(total_precinct),paste(county,"(sp.)"),county)) %>%
+  summarize(Cordray = sum(Cordray), 
+            DeWine = sum(DeWine), 
+            Total = sum(precinct_total)) %>%
+  mutate(ShortName = ifelse(county %in% county[duplicated(county)],paste(county,"(sp.)"),county)) %>%
   rename(County = "county",
-         ShortName = "shortname",
-         HD = "house_district",
-         Cordray = "total_cordray",
-         DeWine = "total_dewine",
-         Total = "total_precinct") %>%
-  select(County,ShortName,HD,Cordray,DeWine,Total)
+         HD = "house_district") %>%
+  select(County,ShortName,HD,Cordray,DeWine,Total) 
 
-### import to TABLE TAB
-write.csv(gov_unsplit_export, file="table_tab_unsplit.csv", row.names = F)
+write.csv(gov_unsplit_df, file="table_tab_unsplit.csv", row.names = F)
 
-county_gov_unsplit <- join_tidy %>%
+
+### SPLIT COUNTIES ###
+county_gov_unsplit_df <- join_tidy %>%
   group_by(county,house_district) %>% 
   select(-c(house_candidate:office)) %>%
-  filter(gov_candidate != "Cordray" & gov_candidate != "DeWine") %>%
+  filter(gov_candidate == "precinct_total") %>%
   distinct() %>%
   filter(!precinct %in% split_precincts) %>%
-  summarize(unsplit_gov_total = sum(as.numeric(gov_votes)))
+  summarize(unsplit_gov_total = sum(as.numeric(gov_votes))) 
 
-county_house_unsplit <- join_tidy %>%
+county_house_unsplit_df <- join_tidy %>%
   group_by(county,house_district) %>% 
   filter(!precinct %in% split_precincts) %>%
   select(-c(office:gov_votes)) %>%
   distinct(precinct,house_candidate, .keep_all = TRUE) %>%
-  summarize(unsplit_house_total = sum(as.numeric(house_votes)))
+  summarize(unsplit_house_total = sum(as.numeric(house_votes))) 
 
-uf <- full_join(county_gov_unsplit,county_house_unsplit, 
-                by=c("county","house_district"))
-uf <- uf %>%
+undervote_df <- full_join(county_gov_unsplit_df,county_house_unsplit_df, 
+                by=c("county","house_district")) %>%
   mutate(undervote_factor = unsplit_house_total/unsplit_gov_total) %>%
   arrange(county,house_district) %>%
   select(-c("unsplit_gov_total","unsplit_house_total")) %>%
-  mutate_at(2, as.character)
+  inner_join(split_precinct_df,undervote_df,
+             by=c("county","house_district"))
 
-split_precinct <- join_tidy %>% 
-  group_by(county,precinct) %>% 
-  select(-c(4:8)) %>%
-  distinct(county,precinct,house_district, .keep_all = TRUE) %>%
-  filter(n()>1) %>%
-  arrange(desc(precinct), .by_group = TRUE) 
-
-split_precinct_uf <- inner_join(split_precinct,uf,
-                                   by=c("county","house_district"))
-
-ga_votes <- ga_tidy %>% #from ohio_tidy2.R
+ga_votes <- join_tidy %>% 
+  select(-c(office:gov_votes)) %>%
   mutate_at(vars(house_votes),funs(as.numeric)) %>%
-  mutate_at(vars(house_district),funs(as.character)) %>%
   group_by(county,precinct,house_district) %>%
   summarize(house_votes = sum(house_votes))
   
-hypothetical <- inner_join(ga_votes,split_precinct_uf,
-                                   by=c("county","precinct","house_district"))
-hypothetical <- hypothetical %>%
+hypothetical_df <- inner_join(ga_votes,undervote_df, by=c("county","precinct","house_district")) %>%
   mutate(hypo_gov_votes = as.integer(house_votes/undervote_factor)) %>%
   group_by(county,precinct) %>%
   mutate(total = sum(hypo_gov_votes)) %>%
   mutate(vote_share = hypo_gov_votes / total)
 
-gov_complete <- join_tidy %>%
-  select(-c(house_district:office)) %>%
-  distinct()
-
-estimated_total <- inner_join(hypothetical,gov_complete, 
-                              by=c("county","precinct"))
-
-estimated_total<- estimated_total %>%
+estimated_vote_df <- inner_join(hypothetical_df,gov_complete_df, 
+                              by=c("county","precinct")) %>%
   mutate(estimated_gov_votes = round(vote_share*gov_votes))
 
-## EXPORT THIS
-county_tabs_split <- estimated_total %>%
+### ADJUSTED NUMBERS TO ADD TO COUNTY TABS ###
+county_tabs_split <- estimated_vote_df %>%
   select(-c(house_votes:vote_share,gov_votes)) %>%
   spread(key=gov_candidate,value=estimated_gov_votes)
 
 write.csv(county_tabs_split, file="county_tabs_split.csv", row.names = F)
 
-complete <- full_join(estimated_total,join_tidy, 
+### SUMMARY TABLE ###
+complete_df <- full_join(estimated_vote_df,join_tidy, 
                       by = c("county", "precinct", "house_district", "house_votes", "gov_candidate", "gov_votes"))
-complete <- complete %>%  
-  select("county","precinct","house_district","gov_candidate","gov_votes","estimated_gov_votes") %>%
+
+by_county <- split(county_tabs_split,county_tabs_split$county)
+sapply(names(by_county), 
+       function (x) write.csv(by_county[[x]], file=paste0("counties/",x,"_split.csv"), row.names=FALSE))
+
+complete_df <- complete_df %>%  
+  select(county,precinct,house_district,gov_candidate,gov_votes,estimated_gov_votes) %>%
   distinct() %>%
   mutate_at(vars(gov_votes),funs(as.numeric)) %>%
   mutate(votes = case_when(!is.na(estimated_gov_votes) ~ estimated_gov_votes,
                            is.na(estimated_gov_votes) ~ gov_votes)) %>%
   distinct(county,precinct,house_district,gov_candidate,gov_votes, .keep_all = TRUE) %>%
-  select(-c("gov_votes","estimated_gov_votes")) %>%
+  select(-c(gov_votes,estimated_gov_votes)) %>%
   spread(key=gov_candidate,value=votes) %>%
   group_by(county,house_district) %>%
-  summarize(total_cordray = sum(Cordray), 
-            total_dewine = sum(DeWine), 
-            total_precinct = sum(precinct_total)) %>%
+  summarize(Cordray = sum(Cordray), 
+            DeWine = sum(DeWine), 
+            Total = sum(precinct_total)) %>%
   ungroup() %>%
-  mutate(cordray_percent = total_cordray/total_precinct,
-         dewine_percent = total_dewine/total_precinct) %>%
-  select(house_district,county,total_cordray,total_dewine,total_precinct,cordray_percent,dewine_percent) %>%
-  rename(HD = "house_district",
-         County = "county",
-         Cordray = "total_cordray",
-         DeWine = "total_dewine",
-         Total = "total_precinct",
-         `Cordray%`= "cordray_percent",
-         `DeWine%` = "dewine_percent")
+  mutate(`Cordray%` = paste0(format(round(Cordray/Total*100,2),nsmall=2),"%"),
+         `DeWine%` = paste0(format(round(DeWine/Total*100,2),nsmall=2),"%")) %>%
+  mutate(ShortName = ifelse(county %in% county[duplicated(county)],paste(county,"(sp.)"),county)) %>%
+  select(house_district,ShortName,Cordray,DeWine,Total,`Cordray%`,`DeWine%`) %>%
+  rename(HD = "house_district") %>%
+  arrange(HD)
 
-write.csv(complete, file="complete_splits.csv", row.names = F)
+write.csv(complete_df, file="ohio_2018_results.csv", row.names = F)
